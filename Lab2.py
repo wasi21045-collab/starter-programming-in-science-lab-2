1faced8eee6e75e760c25957a2e858f31bcd6721
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-01-30 09:02:56 -0500

    Creating Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,11 @@
+# Function 1: Calculate the height of the ball after time t
+# This function should take the initial height h0 and time t as inputs, and return the height at time t.
+def calculate_height(h0, t):
+    # TODO: Implement this function
+    pass  # Replace with your code
+
+# Function 2: Calculate the distance traveled by the car
+# This function should take the time t as input and return the distance traveled by the car.
+def calculate_car_distance(t):
+    # TODO: Implement this function
+    pass  # Replace with your code
