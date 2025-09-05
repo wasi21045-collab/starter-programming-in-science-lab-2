4923a21b0025f0858ab3e177cd4e65c02208553d
Muhammad Wasi Sarfraz <wasi21045@gmail.com>
2025-09-04 23:51:01 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -3,8 +3,11 @@
 # Round up to one decimal point
 def calculate_height(h0, t):
     # TODO: Implement this function
-    pass  # Replace with your code
-
+initial_height = float(input("Enter initial ball height:")
+time = float(input("Enter time: ")
+g = 9.8
+final_height = initial_height - (1/2 * g * t**2)
+print("Height of the ball is", final_height, "meters.")
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
